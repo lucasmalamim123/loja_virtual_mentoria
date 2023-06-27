f91a94f3745526f13b1a5d1845c828ebe319bb50
lucasMalamim <lucas.souza.nextage@gmail.com>
2023-06-26 22:37:39 -0300

    first commmit

---
+++ src/test/java/jdev/mentoria/lojaVirtual/LojaVirtualMentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package jdev.mentoria.lojaVirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualMentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
