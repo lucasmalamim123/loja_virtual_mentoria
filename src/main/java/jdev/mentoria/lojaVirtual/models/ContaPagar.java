27a72880dc363ad55e6e93193592fc14b21992be
lucasMalamim <lucas.souza.nextage@gmail.com>
2023-06-28 23:27:37 -0300

    Continued class creation and files Database.sql

---
+++ src/main/java/jdev/mentoria/lojaVirtual/models/ContaPagar.java
@@ -0,0 +1,119 @@
+package jdev.mentoria.lojaVirtual.models;
+
+import jdev.mentoria.lojaVirtual.enums.StatusContaPagar;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "conta_pagar")
+@SequenceGenerator(name = "seq_conta_pagar", sequenceName = "seq_conta_pagar", initialValue = 1, allocationSize = 1)
+public class ContaPagar implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
+    private Long id;
+
+    private String descricao;
+
+    private BigDecimal valorTotal;
+
+    @Temporal(TemporalType.DATE)
+    private Date dtVencimento;
+
+    @Temporal(TemporalType.DATE)
+    private Date dtPagamento;
+
+    @Enumerated(EnumType.STRING)
+    private StatusContaPagar status;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_forn_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_forn_fk"))
+    private Pessoa pessoaFornecedor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Date getDtVencimento() {
+        return dtVencimento;
+    }
+
+    public void setDtVencimento(Date dtVencimento) {
+        this.dtVencimento = dtVencimento;
+    }
+
+    public Date getDtPagamento() {
+        return dtPagamento;
+    }
+
+    public void setDtPagamento(Date dtPagamento) {
+        this.dtPagamento = dtPagamento;
+    }
+
+    public StatusContaPagar getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusContaPagar status) {
+        this.status = status;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Pessoa getPessoaFornecedor() {
+        return pessoaFornecedor;
+    }
+
+    public void setPessoaFornecedor(Pessoa pessoaFornecedor) {
+        this.pessoaFornecedor = pessoaFornecedor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ContaPagar)) return false;
+        ContaPagar that = (ContaPagar) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
---
+++ src/main/java/jdev/mentoria/lojaVirtual/models/FormaPagamento.java
@@ -0,0 +1,48 @@
+package jdev.mentoria.lojaVirtual.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "forma_pagamento")
+@SequenceGenerator(name = "seq_forma_pagamento", sequenceName = "seq_forma_pagamento", initialValue = 1, allocationSize = 1)
+public class FormaPagamento implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_forma_pagamento")
+    private Long id;
+
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FormaPagamento)) return false;
+        FormaPagamento that = (FormaPagamento) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
