4005c54361606ba8632b3308b1400b787ac9e07b
lucasMalamim <lucas.souza.nextage@gmail.com>
2023-07-04 23:07:29 -0300

    created class StatusRastreio

---
+++ src/main/java/jdev/mentoria/lojaVirtual/models/StatusRastreio.java
@@ -0,0 +1,78 @@
+package jdev.mentoria.lojaVirtual.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "status_rastreio")
+@SequenceGenerator(name = "seq_status_rastreio", sequenceName = "seq_status_rastreio", initialValue = 1, allocationSize = 1)
+public class StatusRastreio implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_status_rastreio")
+    private Long id;
+
+    private String centroDistribuicao;
+
+    private String cidade;
+
+    private String estado;
+
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCentroDistribuicao() {
+        return centroDistribuicao;
+    }
+
+    public void setCentroDistribuicao(String centroDistribuicao) {
+        this.centroDistribuicao = centroDistribuicao;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof StatusRastreio)) return false;
+        StatusRastreio that = (StatusRastreio) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
