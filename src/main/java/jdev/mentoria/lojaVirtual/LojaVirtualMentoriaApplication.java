8b3bd24a662cedc2498687df26c021dae678a114
lucasMalamim <lucas.souza.nextage@gmail.com>
2023-07-10 23:32:49 -0300

    Creating package (repository, service and controller); additional annotations in spring Boot initial class for correction possible future errors

--- src/main/java/jdev/mentoria/lojaVirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/jdev/mentoria/lojaVirtual/LojaVirtualMentoriaApplication.java
@@ -2,8 +2,16 @@ package jdev.mentoria.lojaVirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "jdev.mentoria.lojaVitual.models")
+@ComponentScan(basePackages = {"jdev.*"})
+@EnableJpaRepositories(basePackages = {"jdev.mentoria.lojaVirtual.repository"})
+@EnableTransactionManagement
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
