3ebfed378c14359e5d95518afd588b0558e6a6da
lucasMalamim <lucas.souza.nextage@gmail.com>
2023-07-13 23:00:32 -0300

    Configuration webSecurity for no have problem in the application rest with validations of endpoints(first endPoints, delete and registration in class Acesso)

--- src/main/java/jdev/mentoria/lojaVirtual/controller/AcessoController.java
+++ src/main/java/jdev/mentoria/lojaVirtual/controller/AcessoController.java
@@ -1,19 +1,38 @@
 package jdev.mentoria.lojaVirtual.controller;
 
 import jdev.mentoria.lojaVirtual.models.Acesso;
+import jdev.mentoria.lojaVirtual.repository.AcessoRepository;
 import jdev.mentoria.lojaVirtual.service.AcessoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class AcessoController {
 
     @Autowired
     private AcessoService acessoService;
 
-    @PostMapping("/salvarAcesso")
-    public Acesso salvarAcesso(Acesso acesso){
-        return acessoService.save(acesso);
+    @Autowired
+    private AcessoRepository acessoRepository;
+
+    @ResponseBody /* retorno da api */
+    @PostMapping(value = "**/salvarAcesso") /* mapenado url para receber um JSON */
+    public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso){ /* recebe o JSON e converte para objeto */
+        Acesso acessoSalvo = acessoService.save(acesso);
+        return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
+    }
+
+    @ResponseBody /* retorno da api */
+    @PostMapping(value = "**/deleteAcesso") /* mapenado url para receber um JSON */
+    public ResponseEntity<?> deleteAcesso(@RequestBody Acesso acesso){ /* recebe o JSON e converte para objeto */
+        acessoRepository.deleteById(acesso.getId());
+        return new ResponseEntity<>("ACESSO REMOVIDO COM SUCESSO", HttpStatus.OK);
     }
 }
