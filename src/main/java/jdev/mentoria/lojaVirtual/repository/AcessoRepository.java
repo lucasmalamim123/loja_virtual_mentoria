719c08b77ce03ad9dc6b01f6c3f18c4c2f290fb7
lucasMalamim <lucas.souza.nextage@gmail.com>
2023-07-17 22:10:23 -0300

    Implementation test code (JUnit)

--- src/main/java/jdev/mentoria/lojaVirtual/repository/AcessoRepository.java
+++ src/main/java/jdev/mentoria/lojaVirtual/repository/AcessoRepository.java
@@ -2,11 +2,17 @@ package jdev.mentoria.lojaVirtual.repository;
 
 import jdev.mentoria.lojaVirtual.models.Acesso;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 @Transactional
 public interface AcessoRepository extends JpaRepository<Acesso, Long> {
+
+    @Query("select a from Acesso a where upper(trim(a.descricao)) like %?1%")
+    List<Acesso> buscarAcessoDescricao(String desc);
 
 }
--- src/test/java/jdev/mentoria/lojaVirtual/LojaVirtualMentoriaApplicationTests.java
+++ src/test/java/jdev/mentoria/lojaVirtual/LojaVirtualMentoriaApplicationTests.java
@@ -1,26 +1,73 @@
 package jdev.mentoria.lojaVirtual;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jdev.mentoria.lojaVirtual.controller.AcessoController;
 import jdev.mentoria.lojaVirtual.models.Acesso;
 import jdev.mentoria.lojaVirtual.repository.AcessoRepository;
-import jdev.mentoria.lojaVirtual.service.AcessoService;
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Locale;
+
 @SpringBootTest(classes = LojaVirtualMentoriaApplication.class)
-public class LojaVirtualMentoriaApplicationTests {
+class LojaVirtualMentoriaApplicationTests extends TestCase {
+	@Autowired
+	public AcessoController acessoController;
 
 	@Autowired
-	private AcessoController acessoController;
+	private AcessoRepository acessoRepository;
 
 	@Test
 	public void testCadastraAcesso() {
 		Acesso acesso = new Acesso();
 
-		acesso.setDescricao("ROLE_ADMIN");
+		acesso.setDescricao("lucas");
 
-		acessoController.salvarAcesso(acesso);
+		Assertions.assertNull(acesso.getId());
+
+		/* gravou no banco de dados */
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+
+		assert acesso != null;
+		Assertions.assertTrue(acesso.getId() > 0);
+
+		Assertions.assertEquals("lucas", acesso.getDescricao());
+
+		/* teste de carregamento */
+
+		Acesso acesso2 = acessoRepository.findById(acesso.getId()).get();
+
+		Assertions.assertEquals(acesso.getId(), acesso2.getId());
+
+		/* teste de delete */
+
+		acessoRepository.deleteById(acesso2.getId());
+
+		acessoRepository.flush(); /* rodas sql de delete no banco de dados */
+
+		Acesso acesso3 = acessoRepository.findById(acesso2.getId()).orElse(null);
+
+		Assertions.assertEquals(true, acesso3 == null);
+
+		/* teste de query */
+
+		acesso = new Acesso();
+
+		acesso.setDescricao("ROLE_ALUNO");
+
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+
+		List<Acesso> acessos = acessoRepository.buscarAcessoDescricao("ALUNO".trim().toUpperCase());
+
+
+		Assertions.assertEquals(1, acessos.size());
+
+		assert acesso != null;
+		acessoRepository.deleteById(acesso.getId());
 	}
 
 }
